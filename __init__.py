b8a045c2228ca1fdde435c37605a0de9f786891e
thuhak <thuhak.zhou@nio.com>
2018-12-01 14:11:10 +0800

    change package

--- __init__.py
+++ __init__.py
@@ -1,4 +1,3 @@
-#author: thuhak.zhou@nio.com
 '''
 Common config file loader
 
@@ -7,24 +6,12 @@ when there is include=subpath in config file, take it as sub config.
 auto update when config file has been changed
 
 '''
+#author: thuhak.zhou@nio.com
 try:
-    from inotify_conf import Conf
+    from .inotify_conf import Conf
 except:
-    from conf import Conf
+    from .conf import Conf
 
 
 __all__ = ['Conf']
-
-
-def test():
-    import time
-    import logging
-    logging.basicConfig(level=logging.INFO)
-    conf = Conf('config.json')
-    while True:
-        print(conf)
-        time.sleep(1)
-
-
-if __name__ == '__main__':
-    test()
+__version__ = '1.0.0'
