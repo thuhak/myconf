42ef63240ca3729cf1e0f15b94445f3945a21dd5
thuhak <thuhak.zhou@nio.com>
2019-02-22 10:00:06 +0800

    change setup for platform which does not have inotify support

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
-import os
+import sys
 from setuptools import setup
+from distutils.util import get_platform
 
 
-def openf(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname))
+if sys.version_info < (3, 4):
+    sys.stderr.write('This module requires at least Python 3.4\n')
+    sys.exit(1)
+
+require_libs = ['PyYAML>=3.13']
+
+platform = get_platform()
+if platform.startswith('linux') or platform.startswith('freebsd'):
+    require_libs.append('pyinotify==0.9.6')
 
 
 setup(
     name='myconf',
-    version='1.0.0',
+    version='1.0.1',
     author='thuhak',
     author_email='thuhak.zhou@nio.com',
     description='load json or yaml files as python dict, auto refresh when file changing',
     keywords='config file loader',
     packages =['myconf'],
     url='https://github.com/thuhak/myconf',
-    install_requires=[line.strip() for line in openf("requirements.txt") if line.strip()]
+    install_requires=require_libs
 )
